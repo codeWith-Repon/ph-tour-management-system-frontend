611815f9b2704e2cf672344bd82e5d080c4f3b3d
Md Repon <repon7253@gmail.com>
2025-08-06 21:58:41 +0600

    feat: update index.html to use DOCTYPE declaration and remove dark class from body
    chore: add radix-ui dependency to package.json
    
    feat: refactor App component to use CommonLayout and Outlet for routing
    
    refactor: update main.tsx to use RouterProvider for routing
    
    feat: add Logo component for branding
    
    feat: create AdminLayout component for admin pages
    
    feat: implement CommonLayout component with Navbar and Footer
    
    feat: create Footer component with branding and links
    
    feat: create Navbar component with navigation links and mobile support
    
    chore: add empty AboutHeader and HeroSection components for future use
    
    feat: implement NavigationMenu and Popover components for UI navigation
    
    feat: create About page component with placeholder content
    
    feat: create Analytics page component with placeholder content
    
    feat: set up routing in index.tsx with About page as a child route
    
    chore: remove unused generateRoutes utility file

---
+++ src/components/layout/CommonLayout.tsx
@@ -0,0 +1,19 @@
+import type React from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+
+interface IProps {
+  children: React.ReactNode;
+}
+
+const CommonLayout = ({ children }: IProps) => {
+  return (
+    <div className='min-h-screen flex flex-col'>
+      <Navbar />
+      <div className='grow-1'>{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default CommonLayout;
