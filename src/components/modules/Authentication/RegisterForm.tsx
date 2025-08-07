1159b93a3e04ed5f1346b393b0d689c8dde443e4
Md Repon <repon7253@gmail.com>
2025-08-07 19:29:46 +0600

    ✨ Feature: Add Login and Register pages with forms and images structure

---
+++ src/components/modules/Authentication/RegisterForm.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils';
+import { Link } from 'react-router';
+
+export function RegisterForm({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+ 
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Register your account</h1>
+        <p className="text-sm text-muted-foreground">
+          Enter your details to create an account
+        </p>
+      </div>
+
+
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <Link to="/login" className="underline underline-offset-4">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
