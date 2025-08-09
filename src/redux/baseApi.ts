4f15bd1908acb60af44b659f861200c58667caab
Md Repon <repon7253@gmail.com>
2025-08-09 15:13:11 +0600

    ⚡🔐 🍪 Feature: Enable secure client-side cookie access 🔍✨

--- src/redux/baseApi.ts
+++ src/redux/baseApi.ts
@@ -4,5 +4,9 @@ import axiosBaseQuery from "./axiosBaseQuery"
 export const baseApi = createApi({
     reducerPath: "baseApi",
     baseQuery: axiosBaseQuery(),
+    // baseQuery: fetchBaseQuery({
+    //     baseUrl: config.baseUrl,         ///when you wan't to use fetch base query not axios
+    //     credentials: "include"
+    // }),
     endpoints: () => ({}),
 })
