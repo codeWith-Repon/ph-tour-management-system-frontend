bff8596140fff46e00dcdebb0fd95a5056a88967
Md Repon <repon7253@gmail.com>
2025-08-11 14:30:38 +0600

    ✨ Feature: Implement dynamic sidebar & route generation based on user role 🧩
    - 🛠️ Implemented getSidebarItems util to return sidebar config by role
    - 🆔 Added TRole type and role constants
    - 📋 Created userSidebarItems with bookings page
    - 🔄 Integrated useUserInfoQuery to load sidebar dynamically in AppSidebar
    - 🗂️ Added route generation for user dashboard using generateRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,11 @@ import DashboardLayout from '@/components/layout/DashboardLayout';
 import About from '@/pages/About';
 import Login from '@/pages/Login';
 import Register from '@/pages/Register';
-import bookings from '@/pages/User/bookings';
 import Verify from '@/pages/Verify';
 import { generateRoutes } from '@/utils/generateRoutes';
 import { createBrowserRouter } from 'react-router';
 import { adminSidebarItems } from './adminSidebarItems';
+import { userSidebarItems } from './userSidebarItems';
 
 export const router = createBrowserRouter([
   {
@@ -28,12 +28,7 @@ export const router = createBrowserRouter([
   {
     Component: DashboardLayout,
     path: '/user',
-    children: [
-      {
-        Component: bookings,
-        path: 'bookings',
-      },
-    ],
+    children: [...generateRoutes(userSidebarItems)],
   },
   {
     Component: Login,
--- src/types/index.ts
+++ src/types/index.ts
@@ -30,3 +30,5 @@ export interface ISidebarItem {
         component: ComponentType
     }[]
 }
+
+export type TRole = "SUPER_ADMIN" | "ADMIN" | "USER"
