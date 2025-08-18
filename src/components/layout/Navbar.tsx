c76ad0dd3736e2d4e65e31b135ebd02479f3f334
Md Repon <repon7253@gmail.com>
2025-08-18 12:10:34 +0600

    🔴♻🔴♻ 🔐 feat: add refresh token queue in axios interceptor
    - 🌀 Queues API calls during token refresh to prevent race conditions
    - ✅ Retries failed requests once a new token is issued

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -38,7 +38,7 @@ export default function Navbar() {
   const [logout] = useLogoutMutation();
   const dispatch = useAppDispatch();
 
-  console.log(data);
+  // console.log(data);
   const handleLogout = async () => {
     await logout(undefined);
     dispatch(authApi.util.resetApiState());
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,5 @@
 import config from '@/config'
-import axios from 'axios'
+import axios, { type AxiosRequestConfig } from 'axios'
 
 export const axiosInstance = axios.create({
     baseURL: config.baseUrl,
@@ -16,6 +16,25 @@ axiosInstance.interceptors.request.use(function (config) {
 },
 );
 
+let isRefreshing = false;
+
+let pendingQueue: {
+    resolve: (value: unknown) => void;
+    reject: (value: unknown) => void;
+}[] = [];
+
+const processQueue = (error: unknown) => {
+    pendingQueue.forEach((promise) => {
+        if (error) {
+            promise.reject(error)
+        } else {
+            promise.resolve(null)
+        }
+    })
+
+    pendingQueue = []
+}
+
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
     (response) => {
@@ -23,16 +42,33 @@ axiosInstance.interceptors.response.use(
         return response
     },
     async (error) => {
-        console.log('Request failed', error.response)
+        // console.log('Request failed', error.response)
+        const originalRequest = error.config as AxiosRequestConfig;
+        console.log(originalRequest)
 
         if (error.response.status === 500 && error.response.data.message === "jwt expired") {
             console.log("your token is expired!!!")
 
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    pendingQueue.push({ resolve, reject })
+                })
+                    .then(() => axiosInstance(originalRequest))
+                    .catch((error) => Promise.reject(error))
+            }
+
+            isRefreshing = true
             try {
                 const res = await axiosInstance.post("auth/refresh-token")
                 console.log("new token", res)
+
+                processQueue(null)
+
+                return axiosInstance(originalRequest)
             } catch (error) {
-                console.log(error)
+                return Promise.reject(error)
+            } finally {
+                isRefreshing = false
             }
         }
         // for everything
