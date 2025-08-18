76e2b28cfeb55b7ad213c7974cd5865ccee4d3e8
Md Repon <repon7253@gmail.com>
2025-08-18 10:36:32 +0600

    🔄 fix: implement JWT refresh token logic in axios interceptor
    - ✅ Detects expired tokens and requests a new one
    - 🔐 Updates Authorization header with fresh token

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -17,12 +17,25 @@ axiosInstance.interceptors.request.use(function (config) {
 );
 
 // Add a response interceptor
-axiosInstance.interceptors.response.use(function onFulfilled(response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response;
-}, function onRejected(error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
-});
+axiosInstance.interceptors.response.use(
+    (response) => {
+
+        return response
+    },
+    async (error) => {
+        console.log('Request failed', error.response)
+
+        if (error.response.status === 500 && error.response.data.message === "jwt expired") {
+            console.log("your token is expired!!!")
+
+            try {
+                const res = await axiosInstance.post("auth/refresh-token")
+                console.log("new token", res)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        // for everything
+        return Promise.reject(error)
+    }
+);
